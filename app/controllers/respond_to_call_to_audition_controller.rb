0f77dca86df56963b86be1a1f1bee2ef38548657
Tim Boisvert <boisvert@gmail.com>
2025-08-28 16:13:11 -0500

    Update audition request when returning

--- app/controllers/respond_to_call_to_audition_controller.rb
+++ app/controllers/respond_to_call_to_audition_controller.rb
@@ -5,30 +5,82 @@ class RespondToCallToAuditionController < ApplicationController
   layout "public_facing"
 
   def new
-    @audition_request = AuditionRequest.new
-    @person = @audition_request.build_person
+    # First we'll check if they've already responded to this call to audition
+    if cookies.signed["#{@call_to_audition.hex_code}"]
+
+      # The user has already responded, so look up their Person object and Audution Request
+      @person = Person.find_by(email: cookies.signed["#{@call_to_audition.hex_code}"])
+      @audition_request = @call_to_audition.audition_requests.find_by(person: @person)
+
+      # Get the answers for these questions
+      @answers = {}
+      @questions.each do |question|
+        answer = @audition_request.answers.find_by(question: question)
+        @answers[question.id] = answer.value if answer
+      end
+
+    else
+      # They haven't responded yet, so we'll let them create a new response
+      @audition_request = AuditionRequest.new
+      @person = @audition_request.build_person
+
+      # Empty answers hash
+      @answers = {}
+      @questions.each do |question|
+        @answers[question.id] = ""
+      end
+
+    end
   end
 
   def create
     # Strong parameters for the person
     person_params = params.require(:audition_request).permit(person: [ :stage_name, :email, :pronouns, :socials, :resume, :headshot ])
 
-    # Instantiate the objects
-    @audition_request = AuditionRequest.new
-    @audition_request.call_to_audition = @call_to_audition
-    @person = @audition_request.build_person(person_params[:person])
+    # We may be updating an existing response, so check for that first
+    if cookies.signed["#{@call_to_audition.hex_code}"]
 
-    # Loop through the questions and store the answers
-    params[:question].each do |question_id, value|
-      answer = @audition_request.answers.build
-      answer.question = Question.find question_id
-      answer.value = value
+      # Get the person and audition request
+      @person = Person.find_by(email: cookies.signed["#{@call_to_audition.hex_code}"])
+      @audition_request = @call_to_audition.audition_requests.find_by(person: @person)
+
+      # Update the person with the new details
+      @person.assign_attributes(person_params[:person])
+
+      # Update the answers
+      params[:question].each do |question_id, value|
+        answer = @audition_request.answers.find_or_initialize_by(question: question_id)
+        answer.value = value
+        answer.save!
+      end
+
+    else
+
+      # It's a new request, so instantiate the objects
+      @audition_request = AuditionRequest.new
+      @audition_request.call_to_audition = @call_to_audition
+      @person = @audition_request.build_person(person_params[:person])
+
+      # Loop through the questions and store the answers
+      params[:question].each do |question_id, value|
+        answer = @audition_request.answers.build
+        answer.question = Question.find question_id
+        answer.value = value
+      end
+
     end
 
+    # Assuming they're valid...
     if @audition_request.valid? && @person.valid?
+
+      # ...save them both
       @person.save!
       @audition_request.save!
 
+      # And set a cookie so we know this user has already responded
+      cookies.signed["#{@call_to_audition.hex_code}"] = { value: @person.email, expires: 5.years.from_now }
+
+      # Then send them on to the success page
       redirect_to respond_to_call_to_audition_success_path(hex_code: @call_to_audition.hex_code), status: :see_other
     else
       render :new, status: :unprocessable_entity
