d74c36f91efcf7c0561888a465b7b4c441242d52
Tim Boisvert <boisvert@gmail.com>
2025-09-05 19:01:10 -0500

    Switch to local storage

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -21,7 +21,7 @@ Rails.application.configure do
   # Enable serving of images, stylesheets, and JavaScripts from an asset server.
   # config.asset_host = "http://assets.example.com"
 
-  config.active_storage.service = :amazon
+  config.active_storage.service = :local
 
   # Assume all access to the app is happening through a SSL-terminating reverse proxy.
   config.assume_ssl = true
