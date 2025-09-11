aad8698c48c621b5feddefcba358b4aecd1a75af
Tim Boisvert <boisvert@gmail.com>
2025-09-10 20:52:09 -0500

    Trying to get this db right?

--- db/migrate/20250910211853_change_role_assignment.rb
+++ db/migrate/20250910211853_change_role_assignment.rb
@@ -1,10 +1,10 @@
 class ChangeRoleAssignment < ActiveRecord::Migration[8.0]
   def change
-    remove_reference :show_person_role_assigments, :cast_role, foreign_key: false, index: true, if_exists: true
+    # remove_reference :show_person_role_assigments, :cast_role, foreign_key: false, index: true
 
-    if foreign_key_exists?(:show_person_role_assigments, column: :cast_role_id)
-      remove_foreign_key :show_person_role_assigments, column: :cast_role_id
-    end
+    # if foreign_key_exists?(:show_person_role_assigments, column: :cast_role_id)
+    #   remove_foreign_key :show_person_role_assigments, column: :cast_role_id
+    # end
 
     add_reference :show_person_role_assigments, :role, null: false, foreign_key: true, index: true, if_not_exists: true
   end
