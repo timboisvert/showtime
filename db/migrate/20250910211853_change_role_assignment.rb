602a4c03e5a76676393a23e2b463b77b2828addf
Tim Boisvert <boisvert@gmail.com>
2025-09-10 20:48:15 -0500

    Tweaking migrations

--- db/migrate/20250910211853_change_role_assignment.rb
+++ db/migrate/20250910211853_change_role_assignment.rb
@@ -1,7 +1,11 @@
 class ChangeRoleAssignment < ActiveRecord::Migration[8.0]
   def change
-    remove_reference :show_person_role_assigments, :cast_role, foreign_key: false, index: true
-    remove_foreign_key :show_person_role_assigments, column: :cast_role_id if foreign_key_exists?(:show_person_role_assigments, column: :cast_role_id)
-    add_reference :show_person_role_assigments, :role, null: false, foreign_key: true, index: true
+    remove_reference :show_person_role_assigments, :cast_role, foreign_key: false, index: true, if_exists: true
+
+    if foreign_key_exists?(:show_person_role_assigments, column: :cast_role_id)
+      remove_foreign_key :show_person_role_assigments, column: :cast_role_id
+    end
+
+    add_reference :show_person_role_assigments, :role, null: false, foreign_key: true, index: true, if_not_exists: true
   end
 end
