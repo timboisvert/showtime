8e9da3dc8dffee3974a3adefde334490ec675c1c
Tim Boisvert <boisvert@gmail.com>
2025-09-10 21:01:06 -0500

    More tweaking of migrations

--- db/migrate/20250910211853_change_role_assignment.rb
+++ db/migrate/20250910211853_change_role_assignment.rb
@@ -1,11 +1,11 @@
 class ChangeRoleAssignment < ActiveRecord::Migration[8.0]
   def change
-    # remove_reference :show_person_role_assigments, :cast_role, foreign_key: false, index: true
+    remove_reference :show_cast_assignments, :cast_role, foreign_key: false, index: true
 
-    # if foreign_key_exists?(:show_person_role_assigments, column: :cast_role_id)
-    #   remove_foreign_key :show_person_role_assigments, column: :cast_role_id
-    # end
+    if foreign_key_exists?(:show_cast_assignments, column: :cast_role_id)
+      remove_foreign_key :show_cast_assignments, column: :cast_role_id
+    end
 
-    add_reference :show_person_role_assigments, :role, null: false, foreign_key: true, index: true, if_not_exists: true
+    add_reference :show_cast_assignments, :role, null: false, foreign_key: true, index: true, if_not_exists: true
   end
 end
--- db/migrate/20250910212301_rename_show_cast_assignment.rb
+++ db/migrate/20250910212301_rename_show_cast_assignment.rb
@@ -1,5 +1,5 @@
 class RenameShowCastAssignment < ActiveRecord::Migration[8.0]
   def change
-    rename_table :show_person_role_assigments, :show_person_role_assignments
+    rename_table :show_cast_assignments, :show_person_role_assignments
   end
 end
